bae025e02051af980918efe0e86791c8a7099764
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-04-14 23:54:03 +0300

    Home

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import '../styles/home.css'
+
+import { Col, Container, Row } from "reactstrap";
+import Subtitle from "../shared/Subtitle";
+
+import heroImg from "../assets/images/hero-img01.jpg";
+import heroImg2 from "../assets/images/hero-img02.jpg";
+import heroVideo from "../assets/images/hero-video.mp4";
+import worldImg from "../assets/images/world.png"
+
+const Home = () => {
+    return ( 
+       <>
+
+       {/* Hero section start */}
+       <section>
+        <Container>
+            <Row>
+                <Col lg="6" >
+                    <div className="hero-content">
+                        <div className="hero-subtitle d-flex align-items-center">
+                            <Subtitle subtitle={"Know Before You Go"} />
+                            <img src={worldImg} alt="World" />
+                        </div>
+                        <h1>Travelling opens the door ro creating <span className="highlight">memories</span></h1>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam voluptatum aperiam autem, nulla sunt reprehenderit laboriosam beatae consequuntur, sint accusantium necessitatibus tenetur, voluptates reiciendis consectetur repudiandae nisi. Ullam, inventore incidunt.
+                        </p>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+       </section>
+       {/* Hero section end */}
+
+
+
+       </>
+     );
+}
+ 
+export default Home;
